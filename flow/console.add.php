395a860382b0e316361a437d85ec6246079f6d06
Adam Thomas <thomasadam83@hotmail.com>
2025-11-25 12:24:50 -0500

    Refactor console.add.php: convert to timesheet entry form, update validation, breadcrumbs, and application select.

--- flow/console.add.php
+++ flow/console.add.php
@@ -2,52 +2,46 @@
 
 security_check();
 
-use Endroid\QrCode\QrCode;
-use Endroid\QrCode\Writer\PngWriter;
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 {
 
     // Basic serverside validation
-    if (!validate_blank($_POST['name']) || !validate_blank($_POST['url']))
+    if (!validate_blank($_POST['hours']) || 
+        !validate_blank($_POST['description']) || 
+        !validate_blank($_POST['application_id']) || 
+        !validate_blank($_POST['date']))
     {
-        message_set('QR Code Error', 'There was an error with the provided QR code.', 'red');
+        message_set('Timesheet Entry Error', 'There was an error with the timesheet entry.', 'red');
         header_redirect('/console/add');
     }
 
-    $hash = string_hash(3);
-
-    $qr_code= new QrCode('https://qr.brickmmo.com/'.$hash);
-    $png = new PngWriter();
-    $image = $png->write($qr_code)->getDataUri();
-
     // Save QR code details to the database
-    $query = 'INSERT INTO qrs (
-            name, 
-            url, 
-            image, 
-            hash,
+    $query = 'INSERT INTO hours (
+            date, 
+            hours, 
+            description,
+            application_id,
             user_id,
             created_at,
             updated_at
         ) VALUES (
-            "'.addslashes($_POST['name']).'",
-            "'.addslashes($_POST['url']).'", 
-            "'.$image.'",
-            "'.$hash.'",
+            "'.addslashes($_POST['date']).'",
+            "'.addslashes($_POST['hours']).'", 
+            "'.addslashes($_POST['description']).'",
+            "'.addslashes($_POST['application_id']).'",
             '.$_user['id'].',
             NOW(),
             NOW()
         )';
     mysqli_query($connect, $query);
 
-    message_set('QR Success', 'QR code has been successfully created.');
-    header_redirect('/console/dashboard');
+    message_set('Timesheet Entry Success', 'Timesheet entry has been successfully created.');
+    header_redirect('/console/timesheet/date/'.$_POST['date']);
 }
 
-define('APP_NAME', 'QR Codes');
-define('PAGE_TITLE', 'Add QR Code');
-define('PAGE_SELECTED_SECTION', 'qr-codes');
+define('APP_NAME', 'Flow');
+define('PAGE_TITLE', 'Add Timesheet Entry');
+define('PAGE_SELECTED_SECTION', 'timesheets');
 define('PAGE_SELECTED_SUB_PAGE', '/console/add');
 
 include('../templates/html_header.php');
@@ -58,6 +52,19 @@ include('../templates/main_header.php');
 
 include('../templates/message.php');
 
+$query = 'SELECT user_id,
+    SUM(hours) AS hours_total
+    FROM hours
+    GROUP BY user_id
+    ORDER BY hours_total DESC';
+$result = mysqli_query($connect, $query);
+
+$applications_json = fetch_json('https://applications.brickmmo.com/api/applications/timesheets/true');
+$applications = array();
+foreach($applications_json['applications'] as $application) {
+    $applications[$application['id']] = $application['name'];
+}
+
 ?>
 
 <h1 class="w3-margin-top w3-margin-bottom">
@@ -66,16 +73,17 @@ include('../templates/message.php');
         height="50"
         style="vertical-align: top"
     />
-    QR Codes
+    Flow
 </h1>
 <p>
-    <a href="/console/dashboard">Qr Codes</a> / 
-    Add QR Code
+    <a href="/console/dashboard">Flow</a> / 
+    <a href="/console/timesheet/date/<?=$_GET['date']?>">Flow</a> / 
+    Add Timesheet Enry
 </p>
 
 <hr>
 
-<h2>Add QR Code</h2>
+<h2>Add Timesheet Entry</h2>
 
 <form
     method="post"
@@ -84,30 +92,47 @@ include('../templates/message.php');
 >
 
     <input  
-        name="name" 
+        name="date" 
         class="w3-input w3-border" 
-        type="text" 
-        id="name" 
+        type="date" 
+        id="date" 
         autocomplete="off"
+        value="<?=$_GET['date']?>"
     />
-    <label for="name" class="w3-text-gray">
-        Name <span id="name-error" class="w3-text-red"></span>
+    <label for="date" class="w3-text-gray">
+        Date <span id="date-error" class="w3-text-red"></span>
     </label>
 
     <input  
-        name="url" 
+        name="hours" 
         class="w3-input w3-border w3-margin-top" 
-        type="text" 
-        id="url" 
+        type="number" 
+        id="hours" 
         autocomplete="off"
     />
-    <label for="url" class="w3-text-gray">
-        URL <span id="url-error" class="w3-text-red"></span>
+    <label for="hours" class="w3-text-gray">
+        Hours <span id="hours-error" class="w3-text-red"></span>
+    </label>
+
+    <input  
+        name="description" 
+        class="w3-input w3-border w3-margin-top" 
+        type="text" 
+        id="description" 
+        autocomplete="off"
+    />
+    <label for="description" class="w3-text-gray">
+        Description <span id="description-error" class="w3-text-red"></span>
+    </label>
+
+    <?=form_select_array('application_id', $applications, array('lebel' => 'Application', 'empty_key' => 0, 'empty_value' => ''));?>
+    <label for="application_id" class="w3-text-gray">
+        Application <span id="application-id-error" class="w3-text-red"></span>
     </label>
 
     <button class="w3-block w3-btn w3-orange w3-text-white w3-margin-top" onclick="return validateMainForm();">
         <i class="fa-solid fa-tag fa-padding-right"></i>
-        Add QR Code
+        Add Timesheet Entry
     </button>
 
 </form>
@@ -117,19 +142,33 @@ include('../templates/message.php');
     function validateMainForm() {
         let errors = 0;
 
-        let name = document.getElementById("name");
-        let name_error = document.getElementById("name-error");
-        name_error.innerHTML = "";
-        if (name.value == "") {
-            name_error.innerHTML = "(name is required)";
+        let hours = document.getElementById("hours");
+        let hours_error = document.getElementById("hours-error");
+        hours_error.innerHTML = "";
+        if (hours.value < 1 || hours.value > 12) {
+            hours_error.innerHTML = "(hours is required)";
+            errors++;
+        }
+        else if (!Number.isInteger(Number(hours.value))) {
+            hours_error.innerHTML = "(hours is invalid)";
             errors++;
         }
 
-        let url = document.getElementById("url");
-        let url_error = document.getElementById("url-error");
-        url_error.innerHTML = "";
-        if (url.value == "") {
-            url_error.innerHTML = "(URL is required)";
+        let description = document.getElementById("description");
+        let description_error = document.getElementById("description-error");
+        description_error.innerHTML = "";
+        if (description.value == "") {
+            description_error.innerHTML = "(description is required)";
+            errors++;
+        }
+
+        let application_id = document.getElementById("application_id");
+        let application_id_error = document.getElementById("application-id-error");
+
+        console.log(application_id.value);
+        application_id_error.innerHTML = "";
+        if (application_id.value == 0) {
+            application_id_error.innerHTML = "(application is required)";
             errors++;
         }
 
